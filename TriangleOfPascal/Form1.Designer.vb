98c8d4874b3ad643a7fc74290fde77f99454145b
Eric VONARX <eric.vonarx@free.fr>
2019-10-07 16:30:49 +0200

    Ajoutez des fichiers projet.

---
+++ TriangleOfPascal/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(49, 100)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(717, 539)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(46, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(296, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Pascal's triangle : enter a dimension for it"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(121, 64)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(142, 30)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(425, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 58)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Go (My solution)"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(884, 661)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ TriangleOfPascal/Form1.vb
@@ -0,0 +1,76 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+       
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim iDim As Integer
+
+        iDim = TextBox2.Text
+
+        Dim iResult(iDim) As Integer
+        Dim iPreviousResult(iDim) As Integer
+        Dim sString As String
+
+
+        'Initialisation of the array
+        iResult(0) = 1
+        For iI = 1 To iDim - 1
+            iResult(iI) = 0
+        Next
+
+        For iJ = 0 To iDim - 1
+
+            'Copy the last result in the temporar variable iPreviousResult
+            For iI = 0 To iDim - 1
+                iPreviousResult(iI) = iResult(iI)
+            Next
+
+            'Calculate the Pascal triangle
+            sString = iResult(0)
+            For iI = 1 To iDim - 1
+                iResult(iI) = iPreviousResult(iI) + iPreviousResult(iI - 1)
+                'Store the calculated result for the calculated column
+                If iResult(iI) <> 0 Then
+                    sString = sString & vbTab & +iResult(iI)
+                End If
+            Next
+
+            'Display the calculated result for a whole line
+            TextBox1.Text = TextBox1.Text & sString & vbCrLf
+
+        Next
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Dim iResult(10, 10) As Integer
+        Dim sString As String = ""
+
+
+        For iI = 0 To 9
+            iResult(iI, 1) = 1
+            iResult(iI, iI) = 1
+        Next
+
+        For iLine = 2 To 9
+            For iColumn = 1 To (iLine - 2)
+                iResult(iLine, iColumn) = iResult(iLine - 1, iColumn) + iResult(iLine - 1, iColumn - 1)
+                sString = sString & vbTab & +iResult(iLine, iColumn)
+            Next
+
+            TextBox1.Text = TextBox1.Text & sString & vbCrLf
+            sString = ""
+
+        Next
+
+
+
+
+    End Sub
+End Class
